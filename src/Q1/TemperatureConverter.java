85a0292b2667472dec1f0f8c73b4588304af779d
dgupta-2005 <dgupta0602@gmail.com>
2024-10-14 23:17:12 +0530

    temperature converter code

--- src/Q1/TemperatureConverter.java
+++ src/Q1/TemperatureConverter.java
@@ -1,3 +1,27 @@
 package Q1;
+import java.util.Scanner;
+
+public class TemperatureConverter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter temperature:");
+        double temp = scanner.nextDouble();
+
+        System.out.println("Convert to (1) Fahrenheit or (2) Celsius:");
+        int choice = scanner.nextInt();
+
+        if (choice == 1) {
+            double fahrenheit = (temp * 9/5) + 32;
+            System.out.println("Temperature in Fahrenheit: " + fahrenheit);
+        } else if (choice == 2) {
+            double celsius = (temp - 32) * 5/9;
+            System.out.println("Temperature in Celsius: " + celsius);
+        } else {
+            System.out.println("Invalid choice");
+        }
+        scanner.close();
+    }
+}
 
 
