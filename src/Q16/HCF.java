1cc3ad60ce550fbacf280f5d6ed587bf73e64cf8
dgupta-2005 <dgupta0602@gmail.com>
2024-11-06 14:44:06 +0530

    hcfs

---
+++ src/Q16/HCF.java
@@ -0,0 +1,24 @@
+package Q16;
+import java.util.Scanner;
+
+public class HCF {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the first number:");
+        int num1 = scanner.nextInt();
+        System.out.println("Enter the second number:");
+        int num2 = scanner.nextInt();
+
+        int hcf = 1;
+        for (int i = 1; i <= num1 && i <= num2; i++) {
+            if (num1 % i == 0 && num2 % i == 0) {
+                hcf = i;
+            }
+        }
+
+        System.out.println("HCF of " + num1 + " and " + num2 + " is: " + hcf);
+
+        scanner.close();
+
+    }
+}
