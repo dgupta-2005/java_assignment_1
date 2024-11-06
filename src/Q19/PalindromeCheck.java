b3b24b714decf7d975b9d59f1830fe827c9a15ac
dgupta-2005 <dgupta0602@gmail.com>
2024-11-06 14:47:55 +0530

    palindrome check

---
+++ src/Q19/PalindromeCheck.java
@@ -0,0 +1,29 @@
+package Q19;
+
+import java.util.Scanner;
+
+public class PalindromeCheck {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int number = scanner.nextInt();
+
+        int original = number;
+        int reverse = 0;
+
+        while (number != 0) {
+            int digit = number % 10;
+            reverse = reverse * 10 + digit;
+            number /= 10;
+        }
+
+        if (original == reverse) {
+            System.out.println(original + " is a palindrome.");
+        } else {
+            System.out.println(original + " is not a palindrome.");
+        }
+
+        scanner.close();
+    }
+}
+
