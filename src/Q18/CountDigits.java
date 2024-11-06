9766cac77237d290cbf75a0b97a15439d90fca82
dgupta-2005 <dgupta0602@gmail.com>
2024-11-06 14:47:07 +0530

    digits counter

---
+++ src/Q18/CountDigits.java
@@ -0,0 +1,21 @@
+package Q18;
+
+import java.util.Scanner;
+
+public class CountDigits {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter an integer:");
+        int number = scanner.nextInt();
+
+        int count = 0;
+        while (number != 0) {
+            number /= 10;
+            count++;
+        }
+        System.out.println("Number of digits: " + count);
+
+        scanner.close();
+    }
+}
+
