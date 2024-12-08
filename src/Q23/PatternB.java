397d2c049e138c5cb6770eb89f16506d13a4e2aa
dgupta-2005 <dgupta0602@gmail.com>
2024-12-08 14:56:27 +0530

    pattern printing

---
+++ src/Q23/PatternB.java
@@ -0,0 +1,23 @@
+package Q23;
+//             1
+//            2 1 2
+//          3 2 1 2 3
+//        4 3 2 1 2 3 4
+
+public class PatternB {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= 4 - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = i; j >= 1; j--) {
+                System.out.print(j);
+            }
+            for (int j = 2; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
+
