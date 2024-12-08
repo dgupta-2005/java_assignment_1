733b9714c4efbeca9bd96fca2c092cc2f59d1dbe
dgupta-2005 <dgupta0602@gmail.com>
2024-12-08 15:02:00 +0530

    zero argument Constructor!!

---
+++ src/Q31/ZeroArgConstructor.java
@@ -0,0 +1,12 @@
+package Q31;
+
+class ZeroArgConstructor {
+    ZeroArgConstructor() {
+        System.out.println("0-argument constructor called");
+    }
+
+    public static void main(String[] args) {
+        ZeroArgConstructor obj = new ZeroArgConstructor();
+    }
+}
+
